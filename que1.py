37cc976110aa9041d7abba3758fe6c50f7a2d8da
codewhirl-code <aayushichouksey1507@gmail.com>
2025-12-13 05:14:04 +0530

    code added

---
+++ que1.py
@@ -0,0 +1,4 @@
+t = int(input()) # takes number of testcases
+for _ in range(0,t):
+    w = input() # takes input of word in singular form
+    print(w[:-2]+"oi") # removes us and adds oi converting it into plural form
